5a65d6334fb84f375c2e05eafbf0765a00f6b83a
mulargui <mauricio@ulargui.com>
2021-01-02 12:01:12 -0800

    Env Setup

--- .aws/aws-cli.sh
+++ .aws/aws-cli.sh
@@ -1,7 +1,10 @@
 
-#alias aws='sudo docker run --rm -v $ROOT/aws:/root/.aws -it amazon/aws-cli' 
 
+#use this alias if you want to use the aws cli docker container instead of installing the software
 aws() {
     sudo docker run --rm -v $ROOT/.aws:/root/.aws -v $ROOT:/fs amazon/aws-cli "$@"
 }
-export -f aws
+#export -f aws
+
+#otherwise configure the aws cli (just need to copy the .aws folder to $HOME)
+cp -r .aws $HOME

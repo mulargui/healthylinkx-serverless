aca0af836f2377fb700eba68780bf2c49b5210ba
mulargui <mauricio@ulargui.com>
2021-01-10 19:02:41 -0800

    DB Setup

--- datastore/install.sh
+++ datastore/install.sh
@@ -3,5 +3,7 @@
 #create aurora serverless instance
 aws rds create-db-cluster --db-cluster-identifier healthylinkx-cluster \
 	--engine aurora --engine-mode serverless \
-	#--scaling-configuration MinCapacity=4,MaxCapacity=32,SecondsUntilAutoPause=1000,AutoPause=true \
 	--master-username $DBUSER --master-user-password $DBPWD
+
+
+	#--scaling-configuration MinCapacity=4,MaxCapacity=32,SecondsUntilAutoPause=1000,AutoPause=true \

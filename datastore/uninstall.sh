4d056da4037e45ad9f6f17852f37b3ff632cad0e
mulargui <mauricio@ulargui.com>
2021-01-18 20:04:52 -0800

    RDS Setup

--- datastore/uninstall.sh
+++ datastore/uninstall.sh
@@ -5,5 +5,9 @@ aws rds delete-db-instance --db-instance-identifier healthylinkx-db \
 	--skip-final-snapshot \
 	--delete-automated-backups
 
+#wait till the instance is deleted
+aws rds wait db-instance-deleted \
+    --db-instance-identifier healthylinkx-db
+
 # delete the security group	
 aws ec2 delete-security-group --group-name DBSecGroup

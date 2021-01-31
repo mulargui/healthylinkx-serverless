5a20bd2a284704d4f7d67c21309cac750ad8918f
mulargui <mauricio@ulargui.com>
2021-01-31 08:53:25 -0800

    Providers lambda

--- api/src/providers2.js
+++ api/src/providers2.js
@@ -24,7 +24,7 @@ var db = mysql.createPool({
 exports.handler = async (event) => {
 	return ServerReply (204, event);
 
-/*
+
 	var gender = event.queryStringParameters.[gender];
 	var lastname1 = event.queryStringParameters.[lastname1];
 	var lastname2 = event.queryStringParameters.[lastname2];
@@ -34,7 +34,7 @@ exports.handler = async (event) => {
 	var zipcode = event.queryStringParameters.[zipcode];
  	
 	return ServerReply (204, '#' + gender + '#' + lastname1 + '#' + lastname2 
-		+'#' + lastname3 +'#' + specialty +'#' + distance + '#' + zipcode );*/
+		+'#' + lastname3 +'#' + specialty +'#' + distance + '#' + zipcode );
 	
  	//check params
  	if(!zipcode && !lastname1 && !specialty)
@@ -61,6 +61,7 @@ exports.handler = async (event) => {
  			query += "(Classification = '" + specialty + "')";
 
  	//case 1: no need to calculate zip codes at a distance
+ 	//if (!distance || !zipcode){
  		if(zipcode)
  			if(lastname1 || gender || specialty)
  				query += " AND (Provider_Short_Postal_Code = '"+ zipcode + "')";
@@ -74,4 +75,52 @@ exports.handler = async (event) => {
 		} catch(err) {
 			return ServerReply (500, 'db.query: ' + query + err);
 		}
+	//}
+/*
+ 	//case 2:we need to find zipcodes at a distance
+
+ 	//lets get a few zipcodes
+ 	var queryapi = "/rest/GFfN8AXLrdjnQN08Q073p9RK9BSBGcmnRBaZb8KCl40cR1kI1rMrBEbKg4mWgJk7/radius.json/" + zipcode + "/" + distance + "/mile";
+	var responsestring="";
+
+	var options = {
+  		host: "zipcodedistanceapi.redline13.com",
+  		path: queryapi
+ 	};
+
+	http.request(options, function(res) {
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			responsestring += chunk;
+		});
+
+		res.on('error', function(e) {
+			return ServerReply (500, 'error getting zipcodes!' + e);
+		});	
+
+		res.on('end', function() {
+
+			//no data
+  			if (!responsestring) return ServerReply (204, 'no zipcodes!');
+
+		 	//translate json from string to array
+			var responsejson = JSON.parse(responsestring);
+			var length=responsejson.zip_codes.length;
+
+			//complete the query
+ 			if(lastname1 || gender || specialty)
+ 				query += " AND ((Provider_Short_Postal_Code = '"+responsejson.zip_codes[0].zip_code+"')";
+ 			else
+ 				query += "((Provider_Short_Postal_Code = '"+responsejson.zip_codes[0].zip_code+"')";
+			for (var i=1; i<length;i++){
+ 				query += " OR (Provider_Short_Postal_Code = '"+ responsejson.zip_codes[i].zip_code +"')";
+			}
+  			query += ")) limit 50";
+
+			db.query(query, function(err,results,fields){		
+				if (err) return ServerReply (500, 'db.query failed! ' + query);
+				return ServerReply(200, results);
+			});
+		});
+	}).end();*/		
 }; 

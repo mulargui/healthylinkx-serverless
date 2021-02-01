bd428b1d01068400da821e0fb1e98afebc3f01d1
mulargui <mauricio@ulargui.com>
2021-01-31 18:51:07 -0800

    Transaction lambda

--- api/install.sh
+++ api/install.sh
@@ -120,6 +120,22 @@ aws apigateway put-integration --rest-api-id $APIID --resource-id $RESOURCEID \
 #allow apigateway to call the lambda
 aws lambda add-permission --function-name shortlist --action lambda:InvokeFunction --statement-id api-lambda --principal apigateway.amazonaws.com
 
+#create the resource (transaction)
+aws apigateway create-resource --rest-api-id $APIID --parent-id $PARENTRESOURCEID --path-part transaction
+RESOURCEID=$(aws apigateway get-resources --rest-api-id ${APIID} --query "items[?path=='/transaction'].id")
+
+#create the method (GET)
+aws apigateway put-method --rest-api-id $APIID --resource-id $RESOURCEID --http-method GET --authorization-type NONE
+
+#link the lambda to the method
+LAMBDAARN=$(aws lambda list-functions --query "Functions[?FunctionName==\`transaction\`].FunctionArn")  
+aws apigateway put-integration --rest-api-id $APIID --resource-id $RESOURCEID \
+    --http-method GET --type AWS_PROXY --integration-http-method POST \
+    --uri arn:aws:apigateway:$AWS_REGION:lambda:path/2015-03-31/functions/${LAMBDAARN}/invocations
+
+#allow apigateway to call the lambda
+aws lambda add-permission --function-name transaction --action lambda:InvokeFunction --statement-id api-lambda --principal apigateway.amazonaws.com
+
 #deploy all
 aws apigateway create-deployment --rest-api-id $APIID --stage-name prod
 

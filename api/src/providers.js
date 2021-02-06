4d64a08b5e9ae0c26a58ec86e28ea31864abba9d
mulargui <mauricio@ulargui.com>
2021-02-05 19:39:25 -0800

    adding zipcodes

--- api/src/providers.js
+++ api/src/providers.js
@@ -83,8 +83,7 @@ exports.handler = async (event) => {
 
 	try {
 		const response = await axios.get(queryapi);
-		return ServerReply (200, response.data);
-		//responsestring=response.data.explanation;
+		responsestring=response.data;
 	} catch (err) {
 		return ServerReply (500, {"error": queryapi + ':' + err});
 	}
